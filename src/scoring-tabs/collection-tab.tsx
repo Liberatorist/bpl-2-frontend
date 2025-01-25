1274be119892242c9bd8ad5f8af6e3b4273aa7e0
Liberatorist <Liberatorist@gmail.com>
2025-01-25 11:59:55 +0100

    add images to collection tabs

--- src/scoring-tabs/collection-tab.tsx
+++ src/scoring-tabs/collection-tab.tsx
@@ -5,6 +5,8 @@ import { getSubCategory } from "../types/scoring-category";
 import { red } from "@ant-design/colors";
 import TeamScore from "../components/team-score";
 import { CollectionCardTable } from "../components/collection-card-table";
+import Meta from "antd/es/card/Meta";
+import { getImage } from "../types/scoring-objective";
 
 export function CollectionTab() {
   const { scores, currentEvent } = useContext(GlobalStateContext);
@@ -26,35 +28,73 @@ export function CollectionTab() {
           marginBottom: "20px",
         }}
       >
-        {category.objectives.map((objective) => (
-          <Card
-            key={objective.id}
-            title={
-              <Tooltip title={objective.extra}>
-                <div
-                  style={{
-                    whiteSpace: "normal",
-                    wordWrap: "break-word",
-                    overflowWrap: "break-word",
-                  }}
-                >
-                  {`Collect ${objective.required_number} ${objective.name}`}
-                  {objective.extra ? <a style={{ color: red[6] }}>*</a> : null}
-                </div>
-              </Tooltip>
-            }
-            size="small"
-            styles={{
-              body: {
-                // so that the highlight color goes all the way to the edge
-                paddingLeft: "0px",
-                paddingRight: "0px",
-              },
-            }}
-          >
-            <CollectionCardTable objective={objective} />
-          </Card>
-        ))}
+        {category.objectives.map((objective) => {
+          const img_location = getImage(objective);
+          return (
+            <Card
+              key={objective.id}
+              size="small"
+              styles={{
+                body: {
+                  // so that the highlight color goes all the way to the edge
+                  paddingLeft: "0px",
+                  paddingRight: "0px",
+                },
+              }}
+            >
+              <Meta
+                avatar={
+                  img_location ? (
+                    <div
+                      style={{
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        height: "100%",
+                        maxHeight: "60px",
+                      }}
+                    >
+                      <img
+                        src={img_location}
+                        style={{
+                          height: "100%",
+                          maxHeight: "60px",
+                          width: "auto",
+                        }}
+                      />
+                    </div>
+                  ) : (
+                    ""
+                  )
+                }
+                style={{
+                  height: "100%",
+                  maxHeight: "60px",
+                  width: "auto",
+                  paddingLeft: "10px",
+                  paddingRight: "10px",
+                }}
+                title={
+                  <Tooltip title={objective.extra}>
+                    <div
+                      style={{
+                        whiteSpace: "normal",
+                        wordWrap: "break-word",
+                        overflowWrap: "break-word",
+                      }}
+                    >
+                      {`Collect ${objective.required_number} ${objective.name}`}
+                      {objective.extra ? (
+                        <a style={{ color: red[6] }}>*</a>
+                      ) : null}
+                    </div>
+                  </Tooltip>
+                }
+              />
+              <CollectionCardTable objective={objective} />
+            </Card>
+          );
+        })}
       </div>
     </>
   );
